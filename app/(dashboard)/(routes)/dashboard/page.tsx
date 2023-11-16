fa0affe4c4df8bd3378315bc6e7af4bedc74c831
Yaroslav Volenyuk <volenyuk.yaroslav@gmail.com>
2023-11-16 22:27:06 +0100

    Add clerk auth

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+
+const DashboardPage = () => {
+  return <Button>Click</Button>;
+};
+
+export default DashboardPage;
