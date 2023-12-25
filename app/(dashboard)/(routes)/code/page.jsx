a80d9fbe82d0a092be79b3651ebefa1ce969d7a8
Yaroslav Volenyuk <volenyuk.yaroslav@gmail.com>
2023-12-25 21:59:14 +0100

    Add code generator OpenAI API

--- app/(dashboard)/(routes)/code/page.jsx
+++ app/(dashboard)/(routes)/code/page.jsx
@@ -11,16 +11,17 @@ import UserAvatar from '@/components/user-avatar';
 import { cn } from '@/lib/utils';
 import { zodResolver } from '@hookform/resolvers/zod';
 import axios from 'axios';
-import { MessageSquare } from 'lucide-react';
+import { Code } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import OpenAI from 'openai';
 import { ChatCompletion } from 'openai/resources/index.mjs';
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import ReactMarkdown from 'react-markdown';
 import * as z from 'zod';
 import { formSchema } from './constants';
 
-const ConversationPage = () => {
+const CodePage = () => {
   const router = useRouter();
   const [messages, setMessages] = useState([]);
   const form = useForm({
@@ -39,7 +40,7 @@ const ConversationPage = () => {
       };
 
       const newMessages = [...messages, userMessage];
-      const response = await axios.post('/api/conversation', {
+      const response = await axios.post('/api/code', {
         messages: newMessages,
       });
       setMessages((current) => [...current, userMessage, response.data]);
@@ -55,11 +56,11 @@ const ConversationPage = () => {
   return (
     <div>
       <Heading
-        title="Conversation"
-        description="Our most advanced conversation model."
-        icon={MessageSquare}
-        iconColor="text-violet-500"
-        bgColor="bg-violet-500/10"
+        title="Code generation"
+        description="Generate code using descriptive text"
+        icon={Code}
+        iconColor="text-green-700"
+        bgColor="bg-green-700/10"
       />
       <div className="px-4 lg:px-8 ">
         <div>
@@ -76,7 +77,7 @@ const ConversationPage = () => {
                       <Input
                         className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
                         disabled={isLoading}
-                        placeholder="How do I calculate radius of the Earth?"
+                        placeholder="Simple toggle button using React Hooks."
                         {...field}
                       />
                     </FormControl>
@@ -121,7 +122,42 @@ const ConversationPage = () => {
                 }}
               >
                 {message.role === 'user' ? <UserAvatar /> : <BotAvatar />}
-                <p className="text-sm">{message.content}</p>
+                <ReactMarkdown
+                  className="text-sm overflow-hidden leading-7"
+                  components={{
+                    pre: ({ node, ...props }) => (
+                      <div
+                        style={{
+                          overflow: 'auto' /* overflow-auto */,
+                          width: '100%' /* w-full */,
+                          marginTop:
+                            '0.5rem' /* my-2 (top and bottom margin) */,
+                          marginBottom:
+                            '0.5rem' /* my-2 (top and bottom margin) */,
+                          backgroundColor:
+                            'rgba(122, 122, 122, 0.1)' /* bg-red/10 */,
+                          padding: '0.5rem' /* p-2 */,
+                          borderRadius: '0.5rem' /* rounded-lg */,
+                        }}
+                      >
+                        <pre {...props} />
+                      </div>
+                    ),
+                    code: ({ node, ...props }) => (
+                      <code
+                        style={{
+                          backgroundColor:
+                            'rgba(79, 79, 79, 0.1)' /* bg-black/10 */,
+                          borderRadius: '0.5rem' /* rounded-lg */,
+                          padding: '0.25rem' /* p-1 */,
+                        }}
+                        {...props}
+                      />
+                    ),
+                  }}
+                >
+                  {message.content || ''}
+                </ReactMarkdown>
               </div>
             ))}
           </div>
@@ -131,4 +167,4 @@ const ConversationPage = () => {
   );
 };
 
-export default ConversationPage;
+export default CodePage;
